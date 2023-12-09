54dbe8c37740dd28f3b515c5d3889649c1976ab8
Eduarda Machado <132024978+DudaBittenc@users.noreply.github.com>
2023-12-08 23:36:47 -0300

    Add files via upload

--- Pet-Rescue-main/Pet_Rescue/src/View/lostanimal.php
+++ Pet-Rescue-main/Pet_Rescue/src/View/lostanimal.php
@@ -16,7 +16,6 @@
                     <li><a href="#about">About</a></li>
                     <li><a href="#feature">Features</a></li>
                     <button>Login</button>
-
                 </ul>
             </nav>
             <div class="header-block">
@@ -44,7 +43,7 @@
         </div>
         <div class="card-wrapper">
           <div class="card-item">
-            <img src="../../assets/img/cat.png" alt="Car" />
+            <img src="../../assets/img/cat.png" alt="Cat" />
             <div class="card-content">
               <h3>Audi Supra TT</h3>
               <p>
@@ -55,7 +54,7 @@
             </div>
           </div>
           <div class="card-item">
-            <img src="../../assets/img/cat.png" alt="Car" />
+            <img src="../../assets/img/cat.png" alt="Cat" />
             <div class="card-content">
               <h3>Audi Supra TT</h3>
               <p>
@@ -66,7 +65,7 @@
             </div>
           </div>
           <div class="card-item">
-            <img src="../../assets/img/cat.png" alt="Car" />
+            <img src="../../assets/img/cat.png" alt="Cat" />
             <div class="card-content">
               <h3>Audi Supra TT</h3>
               <p>
@@ -77,7 +76,7 @@
             </div>
           </div>
           <div class="card-item">
-            <img src="../../assets/img/cat.png" alt="Car" />
+            <img src="../../assets/img/cat.png" alt="Cat" />
             <div class="card-content">
               <h3>Audi Supra TT</h3>
               <p>
@@ -88,7 +87,7 @@
             </div>
           </div>
           <div class="card-item">
-            <img src="../../assets/img/cat.png" alt="Car" />
+            <img src="../../assets/img/cat.png" alt="Cat" />
             <div class="card-content">
               <h3>Audi Supra TT</h3>
               <p>
@@ -99,7 +98,7 @@
             </div>
           </div>
           <div class="card-item">
-            <img src="../../assets/img/cat.png" alt="Car" />
+            <img src="../../assets/img/cat.png" alt="Cat" />
             <div class="card-content">
               <h3>Audi Supra TT</h3>
               <p>
@@ -110,7 +109,7 @@
             </div>
           </div>
           <div class="card-item">
-            <img src="../../assets/img/cat.png" alt="Car" />
+            <img src="../../assets/img/cat.png" alt="Cat" />
             <div class="card-content">
               <h3>Audi Supra TT</h3>
               <p>
@@ -121,7 +120,7 @@
             </div>
           </div>
           <div class="card-item">
-            <img src="../../assets/img/cat.png" alt="Car" />
+            <img src="../../assets/img/cat.png" alt="Cat" />
             <div class="card-content">
               <h3>Audi Supra TT</h3>
               <p>
@@ -132,7 +131,7 @@
             </div>
           </div>
           <div class="card-item">
-            <img src="../../assets/img/cat.png" alt="Car" />
+            <img src="../../assets/img/cat.png" alt="Cat" />
             <div class="card-content">
               <h3>Audi Supra TT</h3>
               <p>
@@ -145,5 +144,120 @@
         </div>
       </div>
     </section>
+
+    <section class="about" id="about">
+      <div class="content">
+        <div class="title-wrapper-about">
+          <p>Know about us</p>
+          <h3>About</h3>
+        </div>
+        <div class="about-content">
+          <div class="left">
+            <img src="../../assets/img/Gato-PNG.webp" alt="About" />
+          </div>
+          <div class="right">
+            <h3>Join technology and mobility</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repudiandae ut inventore magni repellendus ab ad itaque facere. A
+              tenetur quam, nobis tempore illum laborum ipsa fuga, itaque
+              possimus repellat ad perspiciatis, velit reiciendis eos facilis
+              sapiente blanditiis quas officia hic beatae quibusdam! Quod
+              eligendi porro possimus voluptatum ad ipsum sapiente soluta,
+              maiores nobis tenetur adipisci officiis nisi illum quae natus
+              nostrum tempora accusantium blanditiis? Rem nesciunt illum
+              dolorum, perferendis quos consequatur suscipit cumque fugiat alias
+              sint repellat qui adipisci error est distinctio doloribus labore
+              sunt modi eos odio quibusdam dicta. Dignissimos voluptate illum
+              possimus quo. Ducimus repellat doloribus quisquam quidem?
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="features" id="features">
+      <div class="content">
+        <div class="title-wrapper-features">
+          <p>what you can do</p>
+          <h3>Features</h3>
+        </div>
+        <div class="feature-card-block">
+          <div class="feature-card-item">
+            <img src="../../assets/img/Dog-png.webp" alt="Feature">
+            <div class="feature-text-content">
+              <h3>Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <div class="feature-card-item">
+            <img src="../../assets/img/Dog-png.webp" alt="Feature">
+            <div class="feature-text-content">
+              <h3>Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <div class="feature-card-item">
+            <img src="../../assets/img/Dog-png.webp" alt="Feature">
+            <div class="feature-text-content">
+              <h3>Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <div class="feature-card-item">
+            <img src="../../assets/img/Dog-png.webp" alt="Feature">
+            <div class="feature-text-content">
+              <h3>Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <div class="feature-card-item">
+            <img src="../../assets/img/Dog-png.webp" alt="Feature">
+            <div class="feature-text-content">
+              <h3>Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <div class="feature-card-item">
+            <img src="../../assets/img/Dog-png.webp" alt="Feature">
+            <div class="feature-text-content">
+              <h3>Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <div class="main">
+        <div class="content footer-links">
+          <div class="footer-company">
+            <h4>company</h4>
+            <h6>About</h6>
+            <h6>Contact</h6>
+          </div>
+          <div class="footer-rental">
+            <h4>rental</h4>
+            <h6>Self-Drive</h6>
+            <h6>Chauffer-Driven</h6>
+            <h6>Help</h6>
+          </div>
+          <div class="footer-social">
+            <h4>Stay connected</h4>
+            <div class="social-icons">
+              <img src="../../assets/img/instagram.png" alt="Instagram">
+              <img src="../../assets/img/facebook.png" alt="Facebook">
+            </div>
+          </div>
+          <div class="footer-contact">
+              <h4>Contact US</h4>
+              <h6>+55 51 98886789</h6>
+              <h6>contato@dev.com.br</h6>
+          </div>
+        </div>
+      </div>
+      <div class="last">Dev 2021 - Use like you want</div>
+    </footer>
 </body>
 </html>
